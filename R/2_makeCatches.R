ee299070ade94bed9254a110d95cd2d66e77bc7a
Maia Kapur <39382798+mkapur-noaa@users.noreply.github.com>
2022-05-31 14:00:04 -0700

    clean up catch x zone plot, auto date

--- R/2_makeCatches.R
+++ R/2_makeCatches.R
@@ -1,23 +1,41 @@
 ## code to extrapolate catches for future years
 ## based on C. Monnohan's file from the 2020 update entitled inputs.R & reports.R
-
 library(dplyr)
 # library(tidyr)
 library(ggplot2)
+theme_set(ggsidekick::theme_sleek())
+theme_update(legend.text = element_text(size = 14), legend.position = 'bottom',
+                     axis.text = element_text(size = 14),axis.title  = element_text(size = 14))
 require(here)
 require(lubridate)
 
+this_year = lubridate::year(Sys.Date())
+last_yr = this_year-1
+date_use <- "2022-05-31" ## dwnld date
+data_folder = here('data','/')
+## Summarise and plot catches by discard/retention/geartype
+gearpal = c('grey22',
+            alpha('grey22',0.65), 
+            'goldenrod',
+            alpha('goldenrod',0.65),
+            'seagreen4',
+            alpha('seagreen4',0.65)) 
 
 
-### ----- Catch from AKFIN -----
-message("Processing catch data...")
-BSAIt <- read.csv(here('data',"2021-10-29-catch.csv")) 
-## Type= (R)etained or (D)iscarded
-g <- BSAIt %>% group_by(YEAR, ZONE, TYPE, GEAR) %>%
+
+BSAIt <- read.csv(paste0(data_folder,date_use,"-catch.csv")) 
+BSAIt %>% 
+  group_by(YEAR, ZONE, TYPE, GEAR) %>%
   summarize(TONS=sum(TONS), .groups='drop') %>%
-  ggplot(aes(YEAR, TONS, color=GEAR)) +
-  geom_line() + facet_grid(ZONE~TYPE, scales='free') + scale_y_log10()
-ggsave(g, file = here('figs',paste0(Sys.Date(),'-catch_by_gear_zone.png')))
+  mutate(lab = paste0(GEAR, ifelse(TYPE == 'D',' Discarded', ' Retained'))) %>%
+  ggplot(aes(YEAR, TONS, color=lab)) +
+  scale_color_manual(values = gearpal)+
+  geom_line() + 
+  labs(x = 'Year', y= 'Catch (tons)', col = '')+ 
+  facet_wrap(GEAR~ZONE, scales = 'free', nrow = 3)  
+
+ggsave(last_plot(), 
+       file = here('figs',paste0(Sys.Date(),'-catch_by_gear_zone.png')))
 
 
 ## Aggregate catch to just year
@@ -30,31 +48,25 @@ SS_catch <- data.frame(year=annual_catch$YEAR, seas=1, fleet=1,
                        catch=annual_catch$total, catch_se=.01)
 write.csv(x=SS_catch, file=here('data',paste0(Sys.Date(),'-SS_catch.csv') ), row.names=FALSE)
 
-## for table - catch by area in 2021
-# tmp0 <- BSAIt %>% 
-#   # filter(year==this_year) %>%
-#   filter(YEAR > 2015) 
-# 
-# tmp0$ZONE <- ifelse(tmp0$ZONE =="EY"|tmp0$ZONE == "SE" |tmp0$ZONE == "WY",
-#                     'EGOA',tmp0$ZONE)
-# 
-# tmp0 %>% group_by(YEAR,ZONE) %>%
-#   summarise(sc=sum(TONS)) %>%
-#   tidyr::pivot_wider(., id_cols = YEAR,
-#                      names_from = ZONE, 
-#                      values_from = sc) %>%
-#   mutate(total = CG+EGOA+WG) %>%
-#   select(YEAR,total, WG, CG, EGOA)
-
-
+## for table - catch by area this year
+tmp0 <- BSAIt %>%
+  filter(YEAR > (this_year-6))
+tmp0 %>% group_by(YEAR,ZONE) %>%
+  summarise(sc=sum(TONS)) %>%
+  tidyr::pivot_wider(., id_cols = YEAR,
+                     names_from = ZONE,
+                     values_from = sc) %>%
+  mutate(total = AI+BS) %>%
+  select(YEAR,total,AI, BS)  %>%
+write.csv(., file=here('data',paste0(Sys.Date(),'-catch_by_zone') ), row.names=FALSE)
 
 ## Inputs for Projections/spp_catch.dat ----
 
 #* catches 2015-2020 ----
 round(tail(SS_catch)) 
 
-#* catches 2021 ----
-this_year= 2021
+#* catches this year ----
+
 # files <- list.files(here('data','weekly_catches'), full.names=TRUE)
 ## For projection model need to predict total catches in this
 ## year. Use weekly catches from from previous years to get
@@ -80,12 +92,20 @@ catch_to_add <- weekly_catches %>% filter(year>=this_year-5 & week > week(today(
   group_by(year) %>% summarize(catch=sum(catch), .groups='drop') %>%
   pull(catch) %>% mean
 message("Precited ", this_year, " catch= ", round(catch_this_year + catch_to_add,0)) ##9272
-## message('Predicted ',this_year, ' catch= ', round(catch_this_year/avg_proportion,0))
-##
 
-#* catches 2022/2023 ----
+#* catches for projection years ----
 ##  use last 5 years' real data average
-SS_catch %>% 
-  filter(year  < 2021& year  > 2015) %>% 
-  summarise(mean(catch )) ## 11140
+projc <- SS_catch %>% 
+  filter(year  < this_year & year  > (this_year-6)) %>% 
+  summarise(mean(catch )) %>% as.numeric()
 
+catchvec = matrix(c((last_yr-1),
+                  last_yr,
+                  this_year:(this_year+2),
+                  SS_catch$catch[SS_catch$year == last_yr-1],
+                  SS_catch$catch[SS_catch$year == last_yr],
+       round(catch_this_year + catch_to_add,0),
+         projc,
+         projc), ncol = 2)
+
+save(catchvec,file = here('data', paste0(Sys.Date(),"-catches_for_proj.rds")))
